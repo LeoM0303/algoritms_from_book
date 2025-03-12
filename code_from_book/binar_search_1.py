8be111cb32d91b8d2617ce0359d4138842de2cc6
LeoM0303 <leomuliarchuk51@gmail.com>
2025-03-12 11:06:23 +0200

    fix problem

--- code_from_book/binar_search_1.py
+++ code_from_book/binar_search_1.py
@@ -4,14 +4,15 @@ def binary_search(lst, item):
 
     while low <= high:
         mid = (low + high) // 2
-        guess = list[mid]
+        guess = lst[mid]
         if guess == item:
             return mid
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
-        return None
+
+    return None
 
 my_list = [1,3,5,7,9]
 
