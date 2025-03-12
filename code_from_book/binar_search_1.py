8d9bf0417d6258fb7e8939269ee42a0e06fcd530
LeoM0303 <leomuliarchuk51@gmail.com>
2025-03-12 11:02:46 +0200

    start

---
+++ code_from_book/binar_search_1.py
@@ -0,0 +1,22 @@
+from django.db.models.fields import return_None
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list)
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+        return None
+
+my_list = [1,3,5,7,9]
+
+print binary_search(my_list, 3)
+print binary_search(my_list, -1)
